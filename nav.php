6279a0a5112042b18763fb2c579d553b35cf37c9
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-24 17:48:57 +0530

    modified

--- nav.php
+++ nav.php
@@ -5,33 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-----Bootstrap-->
     <link rel="stylesheet" href="./resources/bootstrap/bootstrap.min.css">
-
+    <link rel="stylesheet" href="resources/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>HMS</title>
-    <style>
-        .navbar {
-            background-color: #fff;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Adding bottom shadow */
-            margin-left: 0;
-            margin-right: 0;
-        }
-
-        .navbar-brand {
-            color: #333; /* Change brand text color */
-        }
-
-        .navbar-nav {
-            margin-left: auto; /* Pushing nav links to the right */
-        }
-
-        .navbar-nav .nav-link {
-            color: #333; /* Change link text color */
-            margin-left: 20px; /* Adjust spacing between links */
-        }
-
-        .navbar-nav .nav-link:hover {
-            color: #555; /* Change hover color */
-        }
-    </style>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light">
@@ -55,7 +33,7 @@
             <a class="nav-link" href="#">Contact Us</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="#">Login</a>
+            <a class="nav-link" href="./login.php">Login</a>
             </li>
         </ul>
         </div>
