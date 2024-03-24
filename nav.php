f4a8fe893cee635f86591b7cfc0c5a2e88453efb
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-24 13:43:01 +0530

    Added navbar  and its styles

--- nav.php
+++ nav.php
@@ -5,64 +5,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-----Bootstrap-->
     <link rel="stylesheet" href="./resources/bootstrap/bootstrap.min.css">
-    <!-----font awsom link-->
-    <link rel="stylesheet" rel="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" 
-        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" 
-        crossorigin="anonymous" 
-        referrerpolicy="no-referrer" 
-    />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <!----Swiper from CDN-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 
     <title>HMS</title>
+    <style>
+        .navbar {
+            background-color: #fff;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Adding bottom shadow */
+            margin-left: 0;
+            margin-right: 0;
+        }
+
+        .navbar-brand {
+            color: #333; /* Change brand text color */
+        }
+
+        .navbar-nav {
+            margin-left: auto; /* Pushing nav links to the right */
+        }
+
+        .navbar-nav .nav-link {
+            color: #333; /* Change link text color */
+            margin-left: 20px; /* Adjust spacing between links */
+        }
+
+        .navbar-nav .nav-link:hover {
+            color: #555; /* Change hover color */
+        }
+    </style>
 </head>
 <body>
-
-<header>
-        <div class="profile">
-            <img src="photo/logo.png">
-            <h1>SEUSL.</h1>
-            <div class="social-icons">
-                <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
-                <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
-                <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
-                <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
-            </div>
+    <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">SEUSL</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Gallery</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+            </li>
+        </ul>
         </div>
-    
-        <nav>
-            <ul>
-                <li><a href="#home" class="active"><i class="fa-solid fa-house"></i>Home</a></li>
-                <li><a href="reg.html"><i class="fa-solid fa-user"></i>Register</a></li>
-                <li><a href="#"><i class="fa-solid fa-house"></i>Room</a></li>
-                <li><a href="contact.html"><i class="fa-solid fa-envelope"></i>Complient</a></li>
-                <li><a href="#"><i class="fa-solid fa-calendar"></i>Calender</a></li>
-            </ul>
-        </nav>
-    
-    
-        <!-------Footer-->
-        <div class="footer">
-            <p>&copy; Copyright 2024 <br>Designed By <a href="www.codinglovers.com" target="_blank">SEUSL Students.</a> </p>
-    
-        </div>
-    
-        <!-------/Footer-->
-    </header>
-
-<nav class="header">
-        <div class="menu-icon" onclick="openSidebar()">
-            <span class="material-icons-outlined">menu</span>
-        </div>
-        
-        <div class="header-left">
-            <span class="material-icons-outlined">search</span>
-        </div>
-        <div class="header-right">
-            <a href="#"><span class="material-icons-outlined">notifications</span></a>
-            <a href="#"><span class="material-icons-outlined">email</span></a>
-            <a href="#"><span class="material-icons-outlined">account_circle</span></a>
-        </div>
+    </div>
     </nav>
+        
