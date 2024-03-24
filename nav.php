e428d2050ce3c82306cc2fc6a8611ed9b194b4e3
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-24 13:01:38 +0530

    added navbar component

--- nav.php
+++ nav.php
@@ -3,10 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./resources/css/bootstrap.min.css">
-    <title>Document</title>
+    <!-----Bootstrap-->
+    <link rel="stylesheet" href="./resources/bootstrap/bootstrap.min.css">
+    <!-----font awsom link-->
+    <link rel="stylesheet" rel="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" 
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" 
+        crossorigin="anonymous" 
+        referrerpolicy="no-referrer" 
+    />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <!----Swiper from CDN-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <title>HMS</title>
 </head>
 <body>
+
+<header>
+        <div class="profile">
+            <img src="photo/logo.png">
+            <h1>SEUSL.</h1>
+            <div class="social-icons">
+                <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
+                <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+        </div>
     
-</body>
-</html>
+        <nav>
+            <ul>
+                <li><a href="#home" class="active"><i class="fa-solid fa-house"></i>Home</a></li>
+                <li><a href="reg.html"><i class="fa-solid fa-user"></i>Register</a></li>
+                <li><a href="#"><i class="fa-solid fa-house"></i>Room</a></li>
+                <li><a href="contact.html"><i class="fa-solid fa-envelope"></i>Complient</a></li>
+                <li><a href="#"><i class="fa-solid fa-calendar"></i>Calender</a></li>
+            </ul>
+        </nav>
+    
+    
+        <!-------Footer-->
+        <div class="footer">
+            <p>&copy; Copyright 2024 <br>Designed By <a href="www.codinglovers.com" target="_blank">SEUSL Students.</a> </p>
+    
+        </div>
+    
+        <!-------/Footer-->
+    </header>
+
+<nav class="header">
+        <div class="menu-icon" onclick="openSidebar()">
+            <span class="material-icons-outlined">menu</span>
+        </div>
+        
+        <div class="header-left">
+            <span class="material-icons-outlined">search</span>
+        </div>
+        <div class="header-right">
+            <a href="#"><span class="material-icons-outlined">notifications</span></a>
+            <a href="#"><span class="material-icons-outlined">email</span></a>
+            <a href="#"><span class="material-icons-outlined">account_circle</span></a>
+        </div>
+    </nav>
