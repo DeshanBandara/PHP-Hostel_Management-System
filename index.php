a352901568cef0a92bf6577dc836d6b6bf44331d
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-24 13:43:28 +0530

    Added lorem text and title

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php include('./nav.php'); ?>
 
 <!--Body-->
-<div>
-    index page
+<div class='m-5 p-2'>
+    <h1 class="display-1"> Hostel Management System</h1>
+    <p class="text-center p-3 lead"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, 
+        illo quidem! Quasi enim corporis reprehenderit magnam voluptatem eligendi 
+        autem voluptas voluptatum commodi, corrupti itaque neque ipsam pariatur? Sequi, natus quod?
+    </p>
 </div>
 
 <?php include('./footer.php'); ?>
