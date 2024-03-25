92964aebad98bcde66b12173c49f3c399fd2705e
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-25 20:04:48 +0530

    rename variable names

--- login.php
+++ login.php
@@ -8,16 +8,16 @@
       <div class="col-md-6 offset-md-3">
       <a href="./index.php" class='backBtn btn btn-primary'>⬅️ Back</a>
         <h2>Login</h2>
-        <form>
+        <form action="./script.php" method="post">
           <div class='form-group'>
             <label for='email'>Registration Number</label>
-            <input type='text' class='form-control' placeholder='Student registration number' autoComplete='off' name='reg_num'>
+            <input type='text' class='form-control' placeholder='Student registration number' autoComplete='off' name='regNum'>
           </div>
           <div class='form-group'>
             <label for='password'>Password</label>
             <input type='password' class='form-control' placeholder='Enter Password' autoComplete='off' name='password'>
           </div>
-          <a href="./home.php" class='loginBtn btn btn-primary' type='submit'>Login</a><br/>
+          <button class='loginBtn btn btn-primary' type='submit' name='login'>Login</button><br/>
           <div>
             <a href="./register.php" class='regBtn btn btn-secondary'>Register</a>
             <p class='formPara'>Don't have an account</p>
