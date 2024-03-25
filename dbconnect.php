f98a4fe2a33f975f634913d47de61f5a68100bfa
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-25 20:04:27 +0530

    reset database connection

--- dbconnect.php
+++ dbconnect.php
@@ -1,3 +1,9 @@
 <?php 
-    $connect = mysqli_connect("localhost","root","","hostel") or die("DB Connection failed");
+
+    /*$servername='localhost';
+    $username='root';
+    $password='';
+    $dbname='login_system';*/
+
+    $connect=mysqli_connect('localhost','root','','hostel') or die("DB Connection faild");
 ?>
